ff49fa4156d62f868e7c1268c7b910f0443573b2
pratham <pg348@snu.edu.in>
2023-06-21 12:56:02 +0530

    jwt-commit

---
+++ src/main/java/JWT/TOKEN/jwt/controller/HomeController.java
@@ -0,0 +1,57 @@
+package JWT.TOKEN.jwt.controller;
+
+
+import JWT.TOKEN.jwt.model.JwtRequest;
+import JWT.TOKEN.jwt.model.JwtResponse;
+import JWT.TOKEN.jwt.service.UserService;
+import JWT.TOKEN.jwt.utility.JWTUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    private JWTUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public String home() {
+        return "HII I AM PRATHAM :)";
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+
+        final String token =
+                jwtUtility.generateToken(userDetails);
+
+        return  new JwtResponse(token);
+    }
+}
---
+++ src/main/java/JWT/TOKEN/jwt/service/UserService.java
@@ -0,0 +1,25 @@
+package JWT.TOKEN.jwt.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        //Logic to get the user form the Database
+
+        return new User("admin","password",new ArrayList<>());
+    }
+    HashMap<Integer,Integer>map=new HashMap<>();
+    ArrayList<Integer>arr=new ArrayList<>();
+
+}
